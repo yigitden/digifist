c9d43351b284b1e05b31e5ad482cad1ea1338053
yigitden <yigitcandeniz@gmail.com>
2024-03-14 22:15:44 +0300

    fix: fixed function to select color separately for each product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const colors = document.querySelectorAll(".color");
+  const colorSets = document.querySelectorAll(".colors");
 
-  colors.forEach((color) => {
-    color.addEventListener("click", function () {
-      if (!this.classList.contains("color-selected")) {
-        colors.forEach((otherColor) => {
-          otherColor.classList.remove("color-selected");
-        });
-        this.classList.add("color-selected");
-      }
-    });
+  colorSets.forEach((colorSet) => {
+    applyColorSelection(colorSet);
   });
+
+  function applyColorSelection(set) {
+    const colors = set.querySelectorAll(".color");
+
+    colors.forEach((color) => {
+      color.addEventListener("click", function () {
+        if (!this.classList.contains("color-selected")) {
+          colors.forEach((otherColor) => {
+            otherColor.classList.remove("color-selected");
+          });
+          this.classList.add("color-selected");
+        }
+      });
+    });
+  }
 });
