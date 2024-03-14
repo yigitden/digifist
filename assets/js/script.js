df94f17d713e6318d929aba4775424e412ea097b
yigitden <yigitcandeniz@gmail.com>
2024-03-15 01:29:07 +0300

    fix: fixed applyColorSelection function since class name changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const colorSets = document.querySelectorAll(".colors");
+  const colorSets = document.querySelectorAll(".product-card__colors");
 
   colorSets.forEach((colorSet) => {
     applyColorSelection(colorSet);
   });
 
   function applyColorSelection(set) {
-    const colors = set.querySelectorAll(".color");
+    const colors = set.querySelectorAll(".product-card__colors--color");
 
     colors.forEach((color) => {
       color.addEventListener("click", function () {
-        if (!this.classList.contains("color-selected")) {
+        if (!this.classList.contains("product-card__color--selected")) {
           colors.forEach((otherColor) => {
-            otherColor.classList.remove("color-selected");
+            otherColor.classList.remove("product-card__color--selected");
           });
-          this.classList.add("color-selected");
+          this.classList.add("product-card__color--selected");
         }
       });
     });
