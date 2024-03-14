eefe810f0560c303971284cbf86751bb30ac62bb
yigitden <yigitcandeniz@gmail.com>
2024-03-15 01:27:10 +0300

    fix: adjust new setting for slider breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () { 
-  var swiper = new Swiper(".swiper", {
+  new Swiper(".swiper", {
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next"
   },
   breakpoints: {
     320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+      slidesPerView: 2.18,
+      spaceBetween: 12,
     },
     480: {
-      slidesPerView: 3,
-      spaceBetween: 30,
+      slidesPerView: 2.2,
+      spaceBetween: 12,
     },
     640: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+      slidesPerView: 2.2,
+      spaceBetween: 12,
     },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+      slidesPerView: 2.7,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
     },
     1440: {
-      slidesPerView: 2.35,
+      slidesPerView: 2.41,
       spaceBetween: 32,
     },
   },
