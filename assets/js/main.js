c537213e23b1dde1015cafdeb508657706896cc1
yigitden <yigitcandeniz@gmail.com>
2024-03-17 03:19:08 +0300

    style: fixed css for responsive design and settings size of slider items by breakpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () { 
   new Swiper(".swiper", {
-  loop: true,
+  slidesOffsetBefore: 120,
   a11y: true,
   navigation: {
     nextEl: ".swiper-button-next"
   },
   breakpoints: {
     320: {
-      slidesPerView: 2.18,
+      slidesPerView: 2.93,
       spaceBetween: 12,
     },
     480: {
-      slidesPerView: 2.2,
+      slidesPerView: 3,
       spaceBetween: 12,
     },
     749: {
-      slidesPerView: 2.99,
+      slidesPerView: 3,
       spaceBetween: 12,
     },
     1024: {
-      slidesPerView: 2,
+      slidesPerView: 2.2,
       spaceBetween: 16,
     },
     1200: {
-      slidesPerView: 2.4,
+      slidesPerView: 2.8,
       spaceBetween: 20,
     },
     1440: {
-      slidesPerView: 2.41,
+      slidesPerView: 2.87,
       spaceBetween: 32,
     },
   },
